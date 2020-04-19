2d41e99a7235ac27201a76c37b4ec039ba31fb80
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2020-04-19 15:04:22 +0530

    feat(): adds space for Columns :lipstick:

--- src/components/primitives/Columns/index.tsx
+++ src/components/primitives/Columns/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { ViewProps } from "react-native";
 import styled from "styled-components/native";
 import {
   BorderProps,
@@ -9,18 +11,47 @@ import {
   color,
   flexbox,
   layout,
-  space,
+  space as spacing,
 } from "styled-system";
+
+import View from "../View";
 
 export type IColumnsProps = ColorProps | SpaceProps | LayoutProps | FlexboxProps | BorderProps;
 
-const Columns = styled.View<IColumnsProps>`
-  flex-direction: row;
-  ${color}
-  ${space}
-  ${layout}
-  ${flexbox}
-  ${border}
-`;
+const StyledColumns = styled(View)(
+  { flexDirection: "row" },
+  color,
+  spacing,
+  layout,
+  flexbox,
+  border,
+);
+
+type ColumnsProps = IColumnsProps &
+  ViewProps & {
+    children: JSX.Element[] | JSX.Element;
+    space?: number;
+  };
+
+const Columns = ({ space, children, ...props }: ColumnsProps) => {
+  const childrenArray = React.Children.toArray(children);
+  /*
+  | Separate the trailing (not first) children from the children array
+  */
+  const trailingChildren = childrenArray.slice(1);
+  /*
+  | Add the margiin to the children
+  */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const trailingChildrenWithSpacing = trailingChildren.map((child: any) => {
+    return React.cloneElement(child, { ml: space }, null);
+  });
+  /*
+  | New children array with applied margin to trailing children
+  */
+  const computedChildren = [childrenArray[0], trailingChildrenWithSpacing];
+
+  return <StyledColumns {...props}>{computedChildren}</StyledColumns>;
+};
 
 export default Columns;
