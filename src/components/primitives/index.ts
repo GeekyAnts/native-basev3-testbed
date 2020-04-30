b810a95dc52f6b70ac6d0d4626addbad6db7ae8a
Hany Kumar <hanykumar@Hanys-MacBook-Air.local>
2020-04-30 14:27:39 +0530

    chore(icon): code cleaned

--- src/components/primitives/index.ts
+++ src/components/primitives/index.ts
@@ -1,9 +1,22 @@
 import View from "./View";
 import Box, { IBoxProps } from "./Box";
 import Text, { TextProps } from "./Text";
+import styledIcon, { IconProps } from "./Icon";
 import Columns from "./Columns";
 import Column from "./Column";
 import Stack from "./Stack";
 import Inline from "./Inline";
 
-export { View, Text, TextProps, Columns, Column, Box, IBoxProps, Stack, Inline };
+export {
+  View,
+  Text,
+  TextProps,
+  Columns,
+  Column,
+  Box,
+  IBoxProps,
+  Stack,
+  Inline,
+  styledIcon as Icon,
+  IconProps,
+};
