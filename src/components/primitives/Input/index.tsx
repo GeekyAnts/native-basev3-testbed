a9f13d86b006fe71c9c80ba5233d4f727e5f6a84
Hany Kumar <hanykumar@Hanys-MacBook-Air.local>
2020-05-01 14:21:37 +0530

    chore(input): changed fontsize format

--- src/components/primitives/Input/index.tsx
+++ src/components/primitives/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { TextInput, TextProps } from "react-native";
 import {
   BorderProps,
@@ -12,7 +12,7 @@ import {
   space,
   typography,
 } from "styled-system";
-import styled, { ThemeContext } from "styled-components";
+import styled from "styled-components";
 
 type InputProps = TypographyProps &
   LayoutProps &
@@ -27,21 +27,20 @@ type InputProps = TypographyProps &
 const StyledInput = styled(TextInput)<InputProps>(typography, space, border, color, layout);
 
 const Input = ({ placeholder, placeholderTextColor, style, ...props }: InputProps) => {
-  const theme: Theme = useContext(ThemeContext);
   const inputDefaultProps: InputProps = {
     p: 4,
     borderRadius: 4,
     borderWidth: 1,
-    borderColor: theme.colors.gray[6],
-    fontSize: 16,
+    borderColor: "gray.3",
+    fontSize: 2,
   };
 
   return (
     <StyledInput
       placeholder={placeholder}
+      placeholderTextColor={placeholderTextColor}
       {...inputDefaultProps}
       style={style}
-      placeholderTextColor={placeholderTextColor}
       {...props}
     />
   );
